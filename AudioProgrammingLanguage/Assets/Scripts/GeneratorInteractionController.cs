a64e3d8db00e36b57afb7dbb50656b44adf8c3f3
Jack Atherton <atherton.lawrence@gmail.com>
2017-10-03 14:15:27 -0700

    organization and start of palette

---
+++ AudioProgrammingLanguage/Assets/Scripts/GeneratorInteractionController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorInteractionController : MonoBehaviour {
+
+    public PaletteGeneratorController myPalette;
+    private bool myPaletteEnabled = false;
+
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+    private void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+	// Use this for initialization
+	void Start () {
+		myPalette.gameObject.SetActive( myPaletteEnabled );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// send touchpad events
+        if( Controller.GetPressDown( SteamVR_Controller.ButtonMask.Touchpad ) )
+        {
+            //...
+        }
+
+        if( Controller.GetPressDown( SteamVR_Controller.ButtonMask.ApplicationMenu ) )
+        {
+            // ...
+            myPaletteEnabled = !myPaletteEnabled;
+            myPalette.gameObject.SetActive( myPaletteEnabled );
+        }
+	}
+}
---
+++ AudioProgrammingLanguage/Assets/Scripts/PaletteGeneratorController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaletteGeneratorController : MonoBehaviour {
+
+    public Transform[] myCategories;
+
+    private int myCurrentCategory;
+
+	// Use this for initialization
+	void Start()
+    {
+		myCurrentCategory = 0;
+        UpdateCategoryVisualization();
+	}
+
+    public void IncrementCategory()
+    {
+        myCurrentCategory++;
+        myCurrentCategory %= myCategories.Length;
+        UpdateCategoryVisualization();
+    }
+
+    public void DecrementCategory()
+    {
+        myCurrentCategory += myCategories.Length - 1;
+        myCurrentCategory %= myCategories.Length;
+        UpdateCategoryVisualization();
+    }
+
+    private void UpdateCategoryVisualization()
+    {
+        for( int i = 0; i < myCategories.Length; i++ )
+        {
+            myCategories[i].gameObject.SetActive( myCurrentCategory == i );
+        }
+    }
+}
