c211b3e2e236aa12c6bb3cea515f71411cd8b9f4
Jack Atherton <atherton.lawrence@gmail.com>
2018-04-20 14:43:03 -0700

    unity version updated; a few strange behaviors to be investigated at a later date

--- AudioProgrammingLanguage/Assets/Scripts/GlobalUtilty/BillboardController.cs
+++ AudioProgrammingLanguage/Assets/Scripts/GlobalUtilty/BillboardController.cs
@@ -7,6 +7,6 @@ public class BillboardController : MonoBehaviour {
 
 	// Update is called once per frame
 	void LateUpdate () {
-		transform.eulerAngles = new Vector3( 0.0f, InputTracking.GetLocalRotation(VRNode.Head).eulerAngles.y, 0.0f );	
+		transform.eulerAngles = new Vector3( 0.0f, UnityEngine.XR.InputTracking.GetLocalRotation(UnityEngine.XR.XRNode.Head).eulerAngles.y, 0.0f );	
     }
 }
